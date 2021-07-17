cbd36c2ed36b0222ccf4998c54dc43538c89cf14
tubone(Yu Otsubo) <tubo.yyyuuu@gmail.com>
2021-07-17 22:15:06 +0900

    Update screenshot.spec.js

--- cypress/integration/screenshot.spec.js
+++ cypress/integration/screenshot.spec.js
@@ -3,6 +3,7 @@ describe('ScreenShotNetatmoDashboard', () => {
     cy.visit("/");
     cy.wait(10000)
     cy.get('div > span:nth-child(2) > .MuiIconButton-colorInherit:nth-child(1) > .MuiIconButton-label > .MuiSvgIcon-root').click()
+    cy.viewport(1920, 1080);
     cy.screenshot('screenShot',{
       capture: 'fullPage'
     });
